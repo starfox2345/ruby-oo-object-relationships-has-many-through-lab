f72432c56e55eb13a461b4cdc988835361c0691b
yatsun <yatsunlam25@gmail.com>
2021-03-21 21:16:17 -0400

    commit tree 405aa1bd1d47bca3a0b12259aa60efd9771e8e68

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,27 @@
+class Genre
 
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+
+    end
+
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|songs| songs.genre == self}
+    end
+
+    def artists
+        Song.all.select {|songs|
+        songs.genre == self}.map {|instances| instances.artist}
+    end
+
+end
